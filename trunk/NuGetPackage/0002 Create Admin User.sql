f46337eb20a66efa4b9f7d209f9925cf8856a4f5
rakker91_cp <rakker91@users.noreply.github.com>
2013-07-12 00:13:01 +0000

    Added a nuget package to the source.

---
+++ trunk/NuGetPackage/0002 Create Admin User.sql
@@ -0,0 +1,18 @@
+﻿USE [master]
+GO
+
+If not Exists (select Name from sys.sql_logins where name = '$(User)')
+BEGIN
+	CREATE LOGIN [$(User)] WITH PASSWORD=N'$(Password)', DEFAULT_DATABASE=[$(DatabaseName)], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF
+END
+GO
+EXEC master..sp_addsrvrolemember @loginame = N'$(User)', @rolename = N'sysadmin'
+GO
+USE [$(DatabaseName)]
+GO
+CREATE USER [$(User)] FOR LOGIN [$(User)]
+GO
+ALTER USER [$(User)] WITH DEFAULT_SCHEMA=[dbo]
+GO
+EXEC sp_addrolemember N'db_owner', N'$(User)'
+GO
---
+++ trunk/NuGetPackage/Readme.txt
@@ -0,0 +1,2 @@
+﻿Download the MSBuild Extension Pack from here: http://msbuildextensionpack.codeplex.com/releases/view/57599
+Then install the 4.0 versions of the pack.
