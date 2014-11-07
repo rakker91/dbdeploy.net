ad701bde7a95a039a40634a2093f843ba1795c41
MScottReed_cp <mscottreed@users.noreply.github.com>
2014-11-07 18:10:35 +0000

    Add them in the right place

---
+++ trunk/Veracity.Utilities.DatabaseDeploy.Test/Utilities/MockEnvironmentProvider.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Veracity.Utilities.DatabaseDeploy.Utilities;
+
+namespace Veracity.Utilities.DatabaseDeploy.Test.Utilities
+{
+    class MockEnvironmentProvider : EnvironmentProvider
+    {
+        private string userName;
+        private string currentDirectory;
+        private string currentExecutingAssemblyDirectory;
+
+        public MockEnvironmentProvider()
+        {
+            userName = Environment.UserName;
+            currentDirectory = Environment.CurrentDirectory;
+            currentExecutingAssemblyDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        }
+
+        public override string CurrentDirectory
+        {
+            get { return currentDirectory; }
+        }
+
+        public override string UserName
+        {
+            get { return userName; }
+        }
+
+        public override string ExecutingAssemblyDirectory
+        {
+            get { return currentExecutingAssemblyDirectory; }
+        }
+
+        public void SetUserName(string username)
+        {
+            userName = username;
+        }
+        public void SetCurrentDirectory(string currentDir)
+        {
+            currentDirectory = currentDir;
+        }
+        public void SetCurrentExecutingAssemblyDirectory(string dir)
+        {
+            currentExecutingAssemblyDirectory = dir;
+        }
+    }
+}
