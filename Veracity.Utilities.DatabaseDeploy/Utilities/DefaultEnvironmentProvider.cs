a78318106402d5e9399e2fd1413d48d8828ed9b5
MScottReed_cp <mscottreed@users.noreply.github.com>
2014-11-07 18:01:54 +0000

    Check-in missing files

---
+++ Veracity.Utilities.DatabaseDeploy/Utilities/DefaultEnvironmentProvider.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace Veracity.Utilities.DatabaseDeploy.Utilities
+{
+    /// <summary>
+    /// Default Environment provider which uses the stardard .NET Environment class
+    /// </summary>
+    public class DefaultEnvironmentProvider : EnvironmentProvider
+    {
+        /// <summary>
+        /// User standard .NET environment to return current directory
+        /// </summary>
+        public override string CurrentDirectory
+        {
+            get { return Environment.CurrentDirectory; }
+        }
+
+        /// <summary>
+        /// User standard .NET environment to return current user name
+        /// </summary>
+        public override string UserName
+        {
+            get { return Environment.UserName; }
+        }
+
+        /// <summary>
+        /// The location of the currently executing assembly
+        /// </summary>
+        public override string ExecutingAssemblyDirectory
+        {
+            get { return Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location); }
+        }
+    }
+}
---
+++ Veracity.Utilities.DatabaseDeploy/Utilities/EnvironmentProvider.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veracity.Utilities.DatabaseDeploy.Utilities
+{
+    /// <summary>
+    /// Abstract class which represents something that provides the current time
+    /// </summary>
+    public abstract class EnvironmentProvider
+    {
+        private static EnvironmentProvider current;
+
+        static EnvironmentProvider()
+        {
+            current = new DefaultEnvironmentProvider();
+        }
+
+        /// <summary>
+        /// The current TimeProvider
+        /// </summary>
+        public static EnvironmentProvider Current
+        {
+            get { return current; }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("value");
+                }
+                current = value;
+            }
+        }
+
+        /// <summary>
+        /// The current directory
+        /// </summary>
+        public abstract string CurrentDirectory { get; }
+
+        /// <summary>
+        /// The current username
+        /// </summary>
+        public abstract string UserName { get; }
+
+        /// <summary>
+        /// The currently executing assembly name
+        /// </summary>
+        public abstract string ExecutingAssemblyDirectory { get; }
+
+        /// <summary>
+        /// Reset to the default environment provider
+        /// </summary>
+        public static void ResetToDefault()
+        {
+            current = new DefaultEnvironmentProvider();
+        }
+    }
+}
